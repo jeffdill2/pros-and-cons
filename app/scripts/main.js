1e5d0ac027e7547323bf98c9ba44d132f74ea9f9
Jeffrey Dill <jeffdill2@gmail.com>
2014-06-10 22:23:21 -0400

    Created entire app -- was on a role so was a little lax with my commits

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,82 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////// GLOBALS
+////////////////////////////////////////////////////////////
+var prosCollection = new ProsCollection();
+var consCollection = new ConsCollection();
+var neutralsCollection = new NeutralsCollection();
+
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////// PAGE LOAD
+////////////////////////////////////////////////////////////
+$(document).ready(function() {
+	loadArguments();
+});
+
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////// FUNCTIONS
+////////////////////////////////////////////////////////////
+function loadArguments() {
+	prosCollection.fetch().done(function() {
+		prosCollection.each(function(pro) {
+			new ProsView({model: pro});
+		});
+	});
+
+	consCollection.fetch().done(function() {
+		consCollection.each(function(con) {
+			new ConsView({model: con});
+		});
+	});
+
+	neutralsCollection.fetch().done(function() {
+		neutralsCollection.each(function(neutral) {
+			new NeutralsView({model: neutral});
+		});
+	});
+}
+
+$('.new-argument-button').click(function() {
+	var strArgument = $('.new-argument-field').val();
+	var strArgumentType = $('input:radio[name=new-argument-type]:checked').val();
+
+	if (strArgument === '') {
+		alert('You must specify your argument!');
+		return;
+	}
+
+	if (!strArgumentType) {
+		alert('You must specify how you\'d classify your argument!');
+		return;
+	}
+
+	switch(strArgumentType) {
+		case 'pro':
+			var objModel = prosCollection.add({description: strArgument});
+			objModel.save();
+			new ProsView({model: objModel});
+
+			break;
+
+		case 'con':
+			var objModel = consCollection.add({description: strArgument});
+			objModel.save();
+			new ConsView({model: objModel});
+
+			break;
+
+		case 'neutral':
+			var objModel = neutralsCollection.add({description: strArgument});
+			objModel.save();
+			new NeutralsView({model: objModel});
+
+			break;
+
+		default:
+			break;
+	}
+
+	$('.new-argument-field').val('');
+	$('input:radio[name=new-argument-type]').attr('checked', false);
+});
---
+++ app/scripts/neutrals-view.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var NeutralsView = Backbone.View.extend({
+	neutralsTemplate: _.template($('.neutrals-template').text()),
+
+	events: {
+		"click .pro-button" 	: "moveToPros",
+		"click .con-button"		: "moveToCons",
+		"click .delete-button"	: "destroy"
+	},
+
+	initialize: function() {
+		this.listenTo(this.model, 'add change', this.render);
+
+		$('.neutrals').append(this.el);
+
+		this.render();
+	},
+
+	render: function() {
+		var neutralsRendered = this.neutralsTemplate(this.model.attributes);
+		this.$el.html(neutralsRendered);
+	},
+
+	moveToPros: function() {
+		var objModel = prosCollection.add({description: this.$el.find('.description').text()});
+		objModel.save();
+
+		this.model.destroy();
+		this.remove();
+
+		new ProsView({model: objModel});
+	},
+
+	moveToCons: function() {
+		var objModel = consCollection.add({description: this.$el.find('.description').text()});
+		objModel.save();
+
+		this.model.destroy();
+		this.remove();
+
+		new ConsView({model: objModel});
+	},
+
+	destroy: function() {
+		this.model.destroy();
+		this.remove();
+	}
+});
